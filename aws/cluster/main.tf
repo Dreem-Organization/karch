55352be040e053567c25a10f04cfece31c3011ff
Mourad Maatoug <mr.maatoug@gmail.com>
2019-07-22 14:57:35 +0200

    BREAKING CHANGES: Update Terraform to 0.12.x

--- aws/cluster/main.tf
+++ aws/cluster/main.tf
@@ -1,97 +1,114 @@
 // Cluster manifest on S3 and cluster destroy-time provisioner
 resource "aws_s3_bucket_object" "cluster-spec" {
-  bucket = "${var.kops-state-bucket}"
+  bucket = var.kops-state-bucket
   key    = "/karch-specs/${var.cluster-name}/master-cluster-spec.yml"
 
   content = <<EOF
-${join("\n---\n", concat(
-  list(data.template_file.cluster-spec.rendered),
-  data.template_file.master-spec.*.rendered,
-  data.template_file.bastion-spec.*.rendered,
-  list(data.template_file.minion-spec.rendered),
-))}
+${join(
+"\n---\n",
+concat(
+[data.template_file.cluster-spec.rendered],
+data.template_file.master-spec.*.rendered,
+data.template_file.bastion-spec.*.rendered,
+[data.template_file.minion-spec.rendered],
+),
+)}
 EOF
 
-  // On destroy, remove the cluster first, if it exists
-  provisioner "local-exec" {
-    when    = "destroy"
-    command = "(test -z \"$(${var.nodeup-url-env} AWS_SDK_LOAD_CONFIG=1 AWS_PROFILE=${var.aws-profile} kops --state=s3://${var.kops-state-bucket} get cluster | grep ${var.cluster-name})\" ) || ${var.nodeup-url-env} AWS_SDK_LOAD_CONFIG=1 AWS_PROFILE=${var.aws-profile} kops --state=s3://${var.kops-state-bucket} delete cluster --yes ${var.cluster-name}"
-  }
 
-  depends_on = ["aws_route53_record.cluster-root", "aws_vpc.main"]
-}
+    // On destroy, remove the cluster first, if it exists
+    // On destroy, remove the cluster first, if it exists
+    provisioner "local-exec" {
+      when    = destroy
+      command = "(test -z \"$(${var.nodeup-url-env} AWS_SDK_LOAD_CONFIG=1 AWS_PROFILE=${var.aws-profile} kops --state=s3://${var.kops-state-bucket} get cluster | grep ${var.cluster-name})\" ) || ${var.nodeup-url-env} AWS_SDK_LOAD_CONFIG=1 AWS_PROFILE=${var.aws-profile} kops --state=s3://${var.kops-state-bucket} delete cluster --yes ${var.cluster-name}"
+    }
 
-// Cluster create-time provisioner
-resource "null_resource" "kops-cluster" {
-  // Let's dump the cluster spec in a conf file
-  provisioner "local-exec" {
-    command = <<FILEDUMP
+    depends_on = [
+      aws_route53_record.cluster-root,
+      aws_vpc.main,
+    ]
+    }
+
+    // Cluster create-time provisioner
+    resource "null_resource" "kops-cluster" {
+      // Let's dump the cluster spec in a conf file
+      // Let's dump the cluster spec in a conf file
+      provisioner "local-exec" {
+        command = <<FILEDUMP
       cat <<EOF > ${path.module}/${var.cluster-name}-cluster-spec.yml
 ${aws_s3_bucket_object.cluster-spec.content}
 EOF
 FILEDUMP
-  }
 
-  // Let's wait for our newly created DNS zone to propagate
-  provisioner "local-exec" {
-    command = <<EOF
+      }
+
+      // Let's wait for our newly created DNS zone to propagate
+      // Let's wait for our newly created DNS zone to propagate
+      provisioner "local-exec" {
+        command = <<EOF
       until test ! -z "$(dig NS ${var.cluster-name} | grep "ANSWER SECTION")"
       do
         echo "DNS zone ${var.cluster-name} isn't available yet, retrying in 5s"
         sleep 5s
       done
 EOF
-  }
 
-  // Let's register our Kops cluster into remote state
-  provisioner "local-exec" {
-    command = "${var.nodeup-url-env} AWS_SDK_LOAD_CONFIG=1 AWS_PROFILE=${var.aws-profile} kops --state=s3://${var.kops-state-bucket} create -f ${path.module}/${var.cluster-name}-cluster-spec.yml"
-  }
+}
 
-  // Let's remove the cluster spec file from disk
-  provisioner "local-exec" {
-    command = "rm ${path.module}/${var.cluster-name}-cluster-spec.yml"
-  }
+// Let's register our Kops cluster into remote state
+// Let's register our Kops cluster into remote state
+provisioner "local-exec" {
+command = "${var.nodeup-url-env} AWS_SDK_LOAD_CONFIG=1 AWS_PROFILE=${var.aws-profile} kops --state=s3://${var.kops-state-bucket} create -f ${path.module}/${var.cluster-name}-cluster-spec.yml"
+}
 
-  // Do not forget to add our public SSH key over there
-  provisioner "local-exec" {
-    command = "${var.nodeup-url-env} AWS_SDK_LOAD_CONFIG=1 AWS_PROFILE=${var.aws-profile} kops --state=s3://${var.kops-state-bucket} create secret --name ${var.cluster-name} sshpublickey admin -i ${var.admin-ssh-public-key-path}"
-  }
+// Let's remove the cluster spec file from disk
+// Let's remove the cluster spec file from disk
+provisioner "local-exec" {
+command = "rm ${path.module}/${var.cluster-name}-cluster-spec.yml"
+}
 
-  depends_on = ["aws_s3_bucket_object.cluster-spec"]
+// Do not forget to add our public SSH key over there
+// Do not forget to add our public SSH key over there
+provisioner "local-exec" {
+command = "${var.nodeup-url-env} AWS_SDK_LOAD_CONFIG=1 AWS_PROFILE=${var.aws-profile} kops --state=s3://${var.kops-state-bucket} create secret --name ${var.cluster-name} sshpublickey admin -i ${var.admin-ssh-public-key-path}"
+}
+
+depends_on = [aws_s3_bucket_object.cluster-spec]
 }
 
 // Hook for other modules (like instance groups) to wait for the master to be available
 resource "null_resource" "master-up" {
-  provisioner "local-exec" {
-    command = <<EOF
+provisioner "local-exec" {
+command = <<EOF
       until ${var.nodeup-url-env} AWS_SDK_LOAD_CONFIG=1 AWS_PROFILE=${var.aws-profile} kops --state=s3://${var.kops-state-bucket} validate cluster --name ${var.cluster-name}
       do
         echo "Cluster isn't available yet"
         sleep 5s
       done
 EOF
-  }
 
-  depends_on = ["null_resource.kops-cluster"]
+}
+
+depends_on = [null_resource.kops-cluster]
 }
 
 // Hook that triggers cluster updates when the manifest changes
 resource "null_resource" "kops-update" {
-  triggers {
-    cluster_spec = "${aws_s3_bucket_object.cluster-spec.content}"
-  }
+triggers = {
+cluster_spec = aws_s3_bucket_object.cluster-spec.content
+}
 
-  provisioner "local-exec" {
-    command = <<FILEDUMP
+provisioner "local-exec" {
+command = <<FILEDUMP
       cat <<EOF > ${path.module}/${var.cluster-name}-cluster-spec.yml
 ${aws_s3_bucket_object.cluster-spec.content}
 EOF
 FILEDUMP
-  }
 
-  provisioner "local-exec" {
-    command = <<EOF
+      }
+
+      provisioner "local-exec" {
+        command = <<EOF
       ${var.nodeup-url-env} AWS_SDK_LOAD_CONFIG=1 AWS_PROFILE=${var.aws-profile} kops --state=s3://${var.kops-state-bucket} \
         replace -f ${path.module}/${var.cluster-name}-cluster-spec.yml  || exit -1
 
@@ -100,7 +117,9 @@ FILEDUMP
       ${var.nodeup-url-env} AWS_SDK_LOAD_CONFIG=1 AWS_PROFILE=${var.aws-profile} kops --state=s3://${var.kops-state-bucket} \
         update cluster ${var.cluster-name} --yes  || exit -1
 EOF
-  }
 
-  depends_on = ["null_resource.kops-cluster"]
-}
+      }
+
+      depends_on = [null_resource.kops-cluster]
+    }
+
--- aws/cluster/variables.tf
+++ aws/cluster/variables.tf
@@ -1,45 +1,45 @@
 variable "aws-region" {
   description = "The AWS region in which to deploy your cluster & VPC."
-  type        = "string"
+  type        = string
 }
 
 # Networking & Security
 variable "vpc-name" {
   description = "Arbitrary name to give to your VPC"
-  type        = "string"
+  type        = string
 }
 
 variable "vpc-cidr" {
   description = "CIDR of the VPC housing the Kubernetes cluster"
-  type        = "string"
+  type        = string
 }
 
 variable "availability-zones" {
-  type        = "list"
+  type        = list(string)
   description = "Availability zones to span (for HA master deployments, see master-availability-zones)"
 }
 
 variable "multi-natgw" {
-  type        = "string"
+  type        = string
   description = "Boolean that indicates wether or not to create NAT Gateway per Availability Zone (default: true)"
   default     = "true"
 }
 
 variable "kops-topology" {
-  type        = "string"
+  type        = string
   description = "Kops topolopy (public|private), (default: private)"
   default     = "private"
 }
 
 variable "trusted-cidrs" {
-  type        = "list"
+  type        = list(string)
   description = "CIDR whitelist for Kubernetes master HTTPs & bastion SSH access (default: 0.0.0.0/0)"
 
   default = ["0.0.0.0/0"]
 }
 
 variable "admin-ssh-public-key-path" {
-  type        = "string"
+  type        = string
   description = "Path to the cluster admin's public SSH key (default: ~/.ssh/id_rsa.pub)"
 
   default = "~/.ssh/id_rsa.pub"
@@ -48,39 +48,39 @@ variable "admin-ssh-public-key-path" {
 ## DNS
 variable "main-zone-id" {
   description = "Route53 main zone ID (optional if the cluster zone is private)"
-  type        = "string"
+  type        = string
 
   default = ""
 }
 
 variable "create-dns-zone" {
   description = "Route53 main zone ID (optional if the cluster zone is private)"
-  type        = "string"
+  type        = string
 
   default = "true"
 }
 
 variable "cluster-name" {
   description = "Cluster domain name (i.e. mycluster.example.com)"
-  type        = "string"
+  type        = string
 }
 
 variable "kube-dns-domain" {
-  type        = "string"
+  type        = string
   description = "Domain enforced in our cluster by kube-dns (default: cluster.local)."
 
   default = "cluster.local"
 }
 
 variable "dns-provider" {
-  type        = "string"
+  type        = string
   description = "The DNS provider to use (CoreDNS or KubeDNS)"
 
   default = "CoreDNS"
 }
 
 variable "kube-proxy-mode" {
-  type        = "string"
+  type        = string
   description = "Load balancing method for kube-proxy (iptables or ipvs)"
 
   default = "iptables"
@@ -88,143 +88,143 @@ variable "kube-proxy-mode" {
 
 # Kops & Kubernetes
 variable "nodeup-url-env" {
-  type        = "string"
+  type        = string
   description = "NODEUP_URL env. variable override for testing custom builds of nodeup"
 
   default = ""
 }
 
 variable "aws-profile" {
-  type        = "string"
+  type        = string
   description = "Name of the AWS profile in ~/.aws/credentials or ~/.aws/config to use"
 
   default = "default"
 }
 
 variable "kops-state-bucket" {
-  type        = "string"
+  type        = string
   description = "Name of the bucket in which kops stores its state (must be created prior to cluster turnup)"
 }
 
 variable "disable-sg-ingress" {
-  type        = "string"
+  type        = string
   description = "if set to true, a new rule will not be added for each load-balancer service to the nodes security group, you will have to authorize lb->node traffic with a custom security group you add to nodes"
 
   default = "false"
 }
 
 variable "lb-security-group" {
-  type        = "string"
+  type        = string
   description = "if enabled, no SG will be assigned to service E/NLBs and this group will be used instead"
 
   default = ""
 }
 
 variable "etcd-version" {
-  type        = "string"
+  type        = string
   description = "Etcd version to use"
 
   default = "3.1.11"
 }
 
 variable "etcd-enable-tls" {
-  type        = "string"
+  type        = string
   description = "Set to true to enable TLS on etcd containers"
 
   default = "true"
 }
 
 variable "container-networking" {
-  type        = "string"
+  type        = string
   description = "Set the container CNI networking layer (https://github.com/kubernetes/kops/blob/master/docs/networking.md)"
 
   default = "calico"
 }
 
 variable "rbac" {
-  type        = "string"
+  type        = string
   description = "Boolean indicating whether to enable RBAC authorization"
 
   default = "true"
 }
 
 variable "apiserver-runtime-flags" {
-  type        = "map"
+  type        = map(string)
   description = "Map describing the --runtime-config parameter passed to the API server, useful to enable certain alphav2 APIs that aren't integrated in the API server by default, such a batch/v1alpha2 that introduces CronJobs (default: {}). Note: the RBAC flag is automatically set if you enabled RBAC with the 'rbac' variable above"
 
   default = {}
 }
 
 variable "hpa-sync-period" {
-  type        = "string"
+  type        = string
   description = "The frequency at which HPA are evaluated and reconciled"
 
   default = "10s"
 }
 
 variable "hpa-scale-down-delay" {
-  type        = "string"
+  type        = string
   description = "After a downscale, wait at least for this duration before the next downscale"
 
   default = "1m"
 }
 
 variable "hpa-scale-up-delay" {
-  type        = "string"
+  type        = string
   description = "After an upscale, wait at least for this duration before the next scale-out operation"
 
   default = "30s"
 }
 
 variable "oidc-issuer-url" {
-  type        = "string"
+  type        = string
   description = "Setting this to an OIDC Issuer URL will enable OpenID auth with the configured provider"
 
   default = ""
 }
 
 variable "oidc-ca-file" {
-  type        = "string"
+  type        = string
   description = "If using OpendID Connect, the oidc CA file on the APIServer pod, leave empty to use your hosts root CAs (eg.: for SSO with Google)"
 
   default = ""
 }
 
 variable "oidc-client-id" {
-  type        = "string"
+  type        = string
   description = "If using OpendID Connect, the oidc client ID"
 
   default = "example-app"
 }
 
 variable "oidc-username-claim" {
-  type        = "string"
+  type        = string
   description = "If using OpendID Connect, the oidc username claim (default: email)"
 
   default = "email"
 }
 
 variable "oidc-groups-claim" {
-  type        = "string"
+  type        = string
   description = "If using OpendID Connect, the oidc group claim, leave empty if your OIDC provider doesn't provide such a claim (e.g.: Google SSO) or if you don't want to use it"
 
   default = ""
 }
 
 variable "channel" {
-  type        = "string"
+  type        = string
   description = "Channel to use for our Kops cluster (default stable)"
   default     = "stable"
 }
 
 variable "kubernetes-version" {
-  type        = "string"
+  type        = string
   description = "Kubernetes version to use for Core components (default: v1.8.4)"
   default     = "v1.8.4"
 }
 
 variable "cloud-labels" {
-  type        = "map"
+  type        = map(string)
   description = "(Flat) map of kops cloud labels to apply to all resources in cluster"
 
   default = {}
@@ -232,35 +232,35 @@ variable "cloud-labels" {
 
 # Resource reservation on our nodes for Kubernetes daemons & the OS
 variable "kubelet-eviction-flag" {
-  type        = "string"
+  type        = string
   description = "Kubelet flag that configure node memory/storage pod eviction threshold"
 
   default = "memory.available<100Mi,nodefs.available<10%,nodefs.inodesFree<5%,imagefs.available<10%,imagefs.inodesFree<5%"
 }
 
 variable "kube-reserved-cpu" {
-  type        = "string"
+  type        = string
   description = "Amount of CPU reserved for the container runtime & kubelet (default: 50m)"
 
   default = "50m"
 }
 
 variable "kube-reserved-memory" {
-  type        = "string"
+  type        = string
   description = "Amount of CPU reserved for the container runtime & kubelet (default: 256Mi)"
 
   default = "256Mi"
 }
 
 variable "system-reserved-cpu" {
-  type        = "string"
+  type        = string
   description = "Amount of CPU reserved for the operating system (default: 50m)"
 
   default = "50m"
 }
 
 variable "system-reserved-memory" {
-  type        = "string"
+  type        = string
   description = "Amount of CPU reserved for the operating system (default: 100Mi)"
 
   default = "256Mi"
@@ -268,7 +268,7 @@ variable "system-reserved-memory" {
 
 # Systemd/Docker hooks
 variable "hooks" {
-  type        = "list"
+  type        = list(string)
   description = "Docker/Systemd hooks to add to this instance group (add 2 spaces at the beginning of each line for indentation. Also, you'll need the '-' (dash) to indicate that this hook is part of a list."
 
   default = []
@@ -276,99 +276,99 @@ variable "hooks" {
 
 # Master instance group(s)
 variable "master-availability-zones" {
-  type        = "list"
+  type        = list(string)
   description = "Availability zones in which to create master instance groups"
 }
 
 variable "master-lb-visibility" {
-  type        = "string"
+  type        = string
   description = "Visibility (Public|Private) for our Kubernetes masters' ELB (default: Public)"
 
   default = "Public"
 }
 
 variable "master-lb-idle-timeout" {
-  type        = "string"
+  type        = string
   description = "Idle timeout for Kubernetes masters' ELB (default: 300s), in seconds"
   default     = 300
 }
 
 variable "master-additional-sgs" {
-  type        = "list"
+  type        = list(string)
   description = "Security groups to add to our master nodes"
 
   default = []
 }
 
 variable "master-additional-sgs-count" {
-  type        = "string"
+  type        = string
   description = "Number of security groups to add to our master nodes"
 
   default = 0
 }
 
 variable "master-image" {
-  type        = "string"
+  type        = string
   description = "AMI id to use for the master nodes"
 }
 
 variable "master-machine-type" {
-  type        = "string"
+  type        = string
   description = "EC2 instance type to run our masters onto (default: m3.medium)"
 
   default = "c4.large"
 }
 
 variable "master-volume-size" {
-  type        = "string"
+  type        = string
   description = "Size of our master's root volume, in GB (default: 10)"
 
   default = "10"
 }
 
 variable "master-volume-provisioned-iops" {
-  type        = "string"
+  type        = string
   description = "Master volume provisioned IOPS, if applicable"
 
   default = ""
 }
 
 variable "master-volume-type" {
-  type        = "string"
+  type        = string
   description = "Master volume type (io1/gp2), defaults to gp2"
 
   default = "gp2"
 }
 
 variable "master-ebs-optimized" {
-  type        = "string"
+  type        = string
   description = "Boolean (true or false) indicating whether our masters should be EBS optimized"
   default     = "false"
 }
 
 variable "master-update-interval" {
-  type        = "string"
+  type        = string
   description = "Interval (in minutes) between rolling updates of master nodes (default: 8)"
 
   default = "8"
 }
 
 variable "master-cloud-labels" {
-  type        = "map"
+  type        = map(string)
   description = "(Flat) map of EC2 tags to add to master instances"
 
   default = {}
 }
 
 variable "master-node-labels" {
-  type        = "map"
+  type        = map(string)
   description = "(Flat) map of Kubernetes node labels to add to master instances"
 
   default = {}
 }
 
 variable "master-hooks" {
-  type        = "list"
+  type        = list(string)
   description = "Docker/Systemd hooks to add to the master instances only (add 2 spaces at the beginning of each line for indentation. Also, you'll need the '-' (dash) to indicate that this hook is part of a list.)"
 
   default = []
@@ -376,102 +376,102 @@ variable "master-hooks" {
 
 # Bastion instance group
 variable "bastion-image" {
-  type        = "string"
+  type        = string
   description = "AMI id to use for the bastion nodes (in private topology only)"
 }
 
 variable "bastion-additional-sgs" {
-  type        = "list"
+  type        = list(string)
   description = "Security groups to add to our bastion nodes"
 
   default = []
 }
 
 variable "bastion-additional-sgs-count" {
-  type        = "string"
+  type        = string
   description = "Number of security groups to add to our bastion nodes"
 
   default = 0
 }
 
 variable "bastion-machine-type" {
-  type        = "string"
+  type        = string
   description = "EC2 instance type to run our bastions onto (default: t2.micro)"
 
   default = "t2.micro"
 }
 
 variable "bastion-volume-size" {
-  type        = "string"
+  type        = string
   description = "Size of our bastion's root volume, in GB (default: 10)"
 
   default = "10"
 }
 
 variable "bastion-volume-provisioned-iops" {
-  type        = "string"
+  type        = string
   description = "Bastion volume provisioned IOPS, if applicable"
 
   default = ""
 }
 
 variable "bastion-volume-type" {
-  type        = "string"
+  type        = string
   description = "Bastion volume type (io1/gp2), defaults to gp2"
 
   default = "gp2"
 }
 
 variable "bastion-ebs-optimized" {
-  type        = "string"
+  type        = string
   description = "Boolean (true or false) indicating whether our bastion should be EBS optimized"
   default     = "false"
 }
 
 variable "min-bastions" {
-  type        = "string"
+  type        = string
   description = "Bastion ASG min size (default: 1)"
 
   default = 1
 }
 
 variable "max-bastions" {
-  type        = "string"
+  type        = string
   description = "Bastion ASG max size (default: 2)"
 
   default = 2
 }
 
 variable "bastion-update-interval" {
-  type        = "string"
+  type        = string
   description = "Interval (in minutes) between rolling updates of bastion nodes (default: 5)"
 
   default = "5"
 }
 
 variable "bastion-cloud-labels" {
-  type        = "map"
+  type        = map(string)
   description = "(Flat) map of EC2 tags to add to bastion instances"
 
   default = {}
 }
 
 variable "bastion-node-labels" {
-  type        = "map"
+  type        = map(string)
   description = "(Flat) map of Kubernetes node labels to add to bastion instances"
 
   default = {}
 }
 
 variable "bastion-hooks" {
-  type        = "list"
+  type        = list(string)
   description = "Docker/Systemd hooks to add to the bastion instances only (add 2 spaces at the beginning of each line for indentation. Also, you'll need the '-' (dash) to indicate that this hook is part of a list.)"
 
   default = []
 }
 
 variable "bastion-max-price" {
-  type        = "string"
+  type        = string
   description = "Bastion Spot instance max price"
 
   default = ""
@@ -479,143 +479,144 @@ variable "bastion-max-price" {
 
 # Initial minion instance group
 variable "minion-ig-name" {
-  type        = "string"
+  type        = string
   description = "Name to give to the ig created along with the cluster (default: nodes)"
 
   default = "nodes"
 }
 
 variable "minion-ig-public" {
-  type        = "string"
+  type        = string
   description = "Set to true for nodes in the default minion ig to receive a public IP address"
 
   default = "false"
 }
 
 variable "minion-additional-sgs" {
-  type        = "list"
+  type        = list(string)
   description = "Additional security groups to add to our minion nodes"
 
   default = []
 }
 
 variable "minion-additional-sgs-count" {
-  type        = "string"
+  type        = string
   description = "Number of security groups to add to our minion nodes"
 
   default = 0
 }
 
 variable "minion-image" {
-  type        = "string"
+  type        = string
   description = "AMI id to use for the minion nodes (in private topology only)"
 }
 
 variable "minion-machine-type" {
-  type        = "string"
+  type        = string
   description = "EC2 instance type to run our minions onto (default: t2.medium)"
 
   default = "t2.medium"
 }
 
 variable "minion-volume-size" {
-  type        = "string"
+  type        = string
   description = "Size of our default minion ig root volume, in GB (default: 30)"
 
   default = "30"
 }
 
 variable "minion-max-price" {
-  type        = "string"
+  type        = string
   description = "If set, this group will use spot instances with the specified max-price"
 
   default = ""
 }
 
 variable "minion-volume-provisioned-iops" {
-  type        = "string"
+  type        = string
   description = "Minion volume provisioned IOPS, if applicable"
 
   default = ""
 }
 
 variable "minion-volume-type" {
-  type        = "string"
+  type        = string
   description = "Minion volume type (io1/gp2), defaults to gp2"
 
   default = "gp2"
 }
 
 variable "minion-ebs-optimized" {
-  type        = "string"
+  type        = string
   description = "Boolean (true or false) indicating whether our default minion ig should be EBS optimized"
   default     = "false"
 }
 
 variable "min-minions" {
-  type        = "string"
+  type        = string
   description = "Minion ASG min size (default: 1)"
 
   default = 1
 }
 
 variable "max-minions" {
-  type        = "string"
+  type        = string
   description = "Minion ASG max size (default: 3)"
 
   default = 3
 }
 
 variable "minion-taints" {
-  type        = "list"
+  type        = list(string)
   description = "List of taints (under the form key=value) to add to default minion ig"
 
   default = []
 }
 
 variable "minion-update-interval" {
-  type        = "string"
+  type        = string
   description = "Interval (in minutes) between rolling updates of minion nodes (default: 8)"
 
   default = "8"
 }
 
 variable "minion-cloud-labels" {
-  type        = "map"
+  type        = map(string)
   description = "(Flat) map of EC2 tags to add to minion instances"
 
   default = {}
 }
 
 variable "minion-node-labels" {
-  type        = "map"
+  type        = map(string)
   description = "(Flat) map of Kubernetes node labels to add to minion instances"
 
   default = {}
 }
 
 variable "minion-hooks" {
-  type        = "list"
+  type        = list(string)
   description = "Docker/Systemd hooks to add to the minion instances (in the IG created along with the cluster) only (add 2 spaces at the beginning of each line for indentation. Also, you'll need the '-' (dash) to indicate that this hook is part of a list.)"
 
   default = []
 }
 
 variable "master-additional-policies" {
-  type        = "string"
+  type        = string
   description = "Additional IAM policies to add to our master instance role: https://github.com/kubernetes/kops/blob/master/docs/iam_roles.md#adding-additional-policies"
   default     = ""
 }
 
 variable "node-additional-policies" {
-  type        = "string"
+  type        = string
   description = "Additional IAM policies to add to our node instance role: https://github.com/kubernetes/kops/blob/master/docs/iam_roles.md#adding-additional-policies"
   default     = ""
 }
 
 variable "log-level" {
-  type        = "string"
+  type        = string
   description = "V-Log log level of all infrastructure components (APIServer, controller-manager, etc.)"
 
   default = 0
 }
+
